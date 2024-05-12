a508f3aaa61db1510a21591852a49477c91d5777
mazen <mazen.saleh.36@gmail.com>
2024-05-12 20:45:55 +0300

    add auto dowonload to auto dowonload new videos in playlist ,also added path for single dowonload video

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pytube import YouTube
 from bidi.algorithm import get_display
 import arabic_reshaper
 from pytube import Playlist
+import time
 
 # Function to download YouTube video
 def download_video(url):
@@ -21,11 +22,51 @@ def download_video(url):
         print("Video downloaded successfully!")
     except Exception as e:
         print("Error:", str(e))
+
 # Helps to display Arabic text correctly
 def arabic_title(title):
     reshaped_text = arabic_reshaper.reshape(title)
     bidi_text = get_display(reshaped_text)
     return bidi_text
+
+
+def download_playlist_auto(playlist_url, download_path, sleep_interval=2):
+    downloaded_videos = set()
+
+    while True:
+        try:
+            # Create a Playlist object
+            playlist = Playlist(playlist_url)
+
+            # Check if the download path exists, if not, create it
+            if not os.path.exists(download_path):
+                os.makedirs(download_path)
+
+            # Iterate through all videos in the playlist
+            for video in playlist.videos:
+                # If the video has already been downloaded, skip it
+                if video.video_id in downloaded_videos:
+                    continue
+
+                # Get the highest quality audio stream
+                stream = video.streams.get_audio_only()
+                print("downloading new video....")
+                # Download the video to the desktop
+                stream.download(download_path)
+
+                # Add the video to the set of downloaded videos
+                downloaded_videos.add(video.video_id)
+                title = arabic_title(video.title)
+                # Print the title and number of the video
+                print(f"Downloaded video: {title}")
+
+            print("Waiting for new videos...")
+        except Exception as e:
+            print("Error:", str(e))
+
+        # Sleep for the specified interval
+        time.sleep(sleep_interval)
+
 
 def download_playlist(playlist_url, download_path):
     try:
@@ -53,5 +94,5 @@ def download_playlist(playlist_url, download_path):
         print("Error:", str(e))
 
 playlist_url = input("Enter the YouTube playlist URL: ")
-download_playlist(playlist_url, os.path.join(os.path.expanduser("~"), "Desktop"))
+download_playlist_auto(playlist_url, os.path.join(os.path.expanduser("~"), "Desktop"))
 
